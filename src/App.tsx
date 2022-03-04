6f04c75f6294b2fcaa04430057c37c87efdc25c6
咸菜 <lplee7830@gmail.com>
2022-03-04 12:23:42 +0800

    tmp code start eject webpack

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <div className="front-page">
       <Link to="./home">跳转到 home</Link>
       <Link to="./header">跳转到 Header</Link>
     </div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-import Header from "./view/header";
-import Home from "./view/home";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "@/style/index.css";
+import App from "@/App";
+import Header from "@/view/header";
+import Home from "@/view/home";
+import reportWebVitals from "@/reportWebVitals";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+
+function GoHome() {
+  return (
+    <div>
+      <Link to="/"> 跳转到首页</Link>
+    </div>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+      <GoHome />
       <Routes>
         <Route path="/" element={<App />}></Route>
         <Route path="/home" element={<Home />}></Route>
-        <Route path="/about" element={<Header />}></Route>
+        <Route path="/header" element={<Header />}></Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
